b6403ac6dbb80fff8d82855e4ce2bc452c15be50
David Gomez <davidgomezarg@gmail.com>
2023-12-30 17:20:27 -0300

    Se agregó metodo fetch para darle funcionalidad al boton de la view /producto

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -51,6 +51,13 @@ router.get("/products",async(req,res)=>{
             result.msg.nextPage =`http://localhost:8080/products?page=${options.page +1 }`;
         }
 
+    
+    // Agregamos a cada producto el cid, porque tube problemas con handlebars al leer la propiedad de otras formas.
+    for(const data of result.msg.docs)
+    {
+        data.cid="658cbaa3b299fdafc649721c";
+    }
+
     console.log(result.msg);
     res.render("products",{msg:result.msg})
 
